7ffbb7f7d08b3553e4ed4d77b32b50bc8036486b
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-10-03 09:36:22 -0400

    using BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import { Button } from './components/Button';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variante="primary" />
-      <Button variante="danger" />
-      <Button variante="secondary" />
-      <Button variante="alert" />
-      <Button />
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
