08ada57b78ff9c4021efc8076ca2c777a96a3145
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-10-13 17:50:45 -0400

    a

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,14 +18,25 @@ export function Home() {
           <TaskInput
             id="task"
             type="text"
+            list="tasksuggestion"
             placeholder="De um nome para o seu projeto"
           />
+
+          <datalist id="tasksuggestion">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
