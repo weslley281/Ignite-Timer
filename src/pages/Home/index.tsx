710509a5139db8faaa8e51a9659e59375986cc0a
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-20 06:35:25 -0400

    a

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from 'phosphor-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   CountdownContainer,
   FormContainer,
@@ -12,6 +12,7 @@ import {
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as zod from 'zod';
+import { differenceInSeconds } from 'date-fns';
 
 const newCircleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a Tarefa'),
@@ -28,6 +29,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
 }
 
 export function Home() {
@@ -40,21 +42,32 @@ export function Home() {
     defaultValues: { task: '', minutesAmount: 0 },
   });
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+  }, [activeCycle]);
+
   function handleCreateNewCicle(data: newCircleFormData) {
     const id = String(new Date().getTime());
 
     const newCicle: Cycle = {
       id,
-      minutesAmount: data.minutesAmount,
       task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     };
 
     setCycles((state) => [...state, newCicle]);
     setActiveCycleId(id);
     reset();
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
