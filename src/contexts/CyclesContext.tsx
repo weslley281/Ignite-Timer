5f112ab5a29462c302d1cabc7f3066812c9925db
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-30 07:42:58 -0400

    a

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,11 @@
 import { createContext, ReactNode, useState, useReducer } from 'react';
-import { ActionTypes, cyclesReduce } from '../reducers/cycles';
+import {
+  ActionTypes,
+  addNewCycleAction,
+  InterruptCurrentCycleAction,
+  markCurrentCycleAsFineshedAction,
+} from '../reducers/cycles/actions';
+import { cyclesReduce } from '../reducers/cycles/reducer';
 
 interface CreateCycleData {
   task: string;
@@ -46,12 +52,7 @@ export function CyclesContextProvider({
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   function markCurrentCycleAsFinishide() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(markCurrentCycleAsFineshedAction());
   }
 
   function setSecondsPassed(seconds: number) {
@@ -68,23 +69,13 @@ export function CyclesContextProvider({
       startDate: new Date(),
     };
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      },
-    });
+    dispatch(addNewCycleAction(newCycle));
 
     setAmountSecondsPassed(0);
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(InterruptCurrentCycleAction());
   }
 
   return (
--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,15 +1,12 @@
-import { Cycle } from '../contexts/CyclesContext';
+import { Cycle } from '../../contexts/CyclesContext';
+import { ActionTypes } from './actions';
 
 interface CyclesState {
   cycles: Cycle[];
   activeCycleId: string | null;
 }
 
-export enum ActionTypes {
-  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
-  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
-  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
-}
+//
 
 export function cyclesReduce(state: CyclesState, action: any) {
   switch (action.type) {
